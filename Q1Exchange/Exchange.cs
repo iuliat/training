a3249e5b3ae63ef1b9f1638f9436164247cc5239
Simona Iuliana Toader <iulia.s.toader@gmail.com>
2017-03-16 16:50:28 +0200

    Exchange added

---
+++ Q1Exchange/Exchange.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1Exchange
+{
+    public class Exchange
+    {
+        public int MakeChange(int amount)
+        {
+            int[] bills = { 50, 20, 10, 5, 1, 100 };
+
+            // Sort the bills descending by their value
+            Array.Sort(bills);
+            Array.Reverse(bills);
+
+            if(amount <= 0)
+            {
+                throw new Exception("Amount inserted must be > 0.");
+            }
+
+
+            int result = 0;
+            foreach(int bill in bills)
+            {
+                int no_bills = amount / bill;
+                amount = amount - no_bills * bill;
+                result += no_bills;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Q1ExchangeTests/ExchangeTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Q1Exchange;
+using Q1ExchangeTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Q1ExchangeTests
+{
+    [TestClass]
+    public class ExchangeTests
+    {
+        [TestMethod]
+        public void MakeChangePositiveAmount()
+        {
+            Exchange exhange = new Exchange();
+            int actualResult = exhange.MakeChange(135);
+            int expectedResult = 4;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Amount inserted must be > 0.")]
+        public void MakeChangeNegativeAmount()
+        {
+            Exchange exhange = new Exchange();
+            int actualResult = exhange.MakeChange(-10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Amount inserted must be > 0.")]
+        public void MakeChangeZeroAmount()
+        {
+            Exchange exhange = new Exchange();
+            int actualResult = exhange.MakeChange(0);
+        }
+    }
+}
